cc55bf5ae455bf9fe93d1e3b4eab279f3e5cd8af
Alec Dergachev <alecwithac27@gmail.com>
2022-12-09 21:09:36 -0500

    added the functions for the modal
    added functions for activating and deactivating the modal

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 window.onload = () => {
-    let menuActivate = document.getElementById(`menu`);
+    let body = document.querySelector(`body`);
+    let menuActivate = document.getElementById(`menu`); // selects the menu button
     let menu = document.querySelector(`nav`);
-    // let modalActivate = document.getElementById(`modal`); // selecting my 2 buttons
+    let modalActivate = document.getElementById(`modal`); // selects the modal button
+    let modalSpace = document.querySelectorAll(`div`)[0];
 
-    // the below functions enable user interaction
+    // the below function activates the menu when the button is clicked
     menuActivate.addEventListener(`click`, () => {
         if (!menu.classList.contains(`hidden`)) {
             menu.classList.add(`hidden`);
@@ -12,7 +14,18 @@ window.onload = () => {
         }
     });
 
-    // modalActivate.addEventListener(`click`, () => {
-
-    // });
+    // This activates the modal when the button is clicked on
+    modalActivate.addEventListener(`click`, () => {
+        if (modalSpace.classList.contains(`hidden`)) {
+            modalSpace.classList.remove(`hidden`);
+            body.classList.add(`no-scroll`);
+        }
+    });
+    // This deactivates the modal when the user clicks outside the content panel
+    modalSpace.addEventListener(`click`, () => {
+        if (!modalSpace.classList.contains(`hidden`)) {
+            modalSpace.classList.add(`hidden`);
+            body.classList.remove(`no-scroll`);
+        }
+    });
 };
